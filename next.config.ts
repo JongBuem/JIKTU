42cacd5658ba4b7bbf7d24d199ed799004d74230
joung bueam <jbryu@cloocus.com>
2025-04-17 14:14:02 +0900

    feat: next + typescript + jest 설정

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: false, //개발 모드 하단의 Next.js 배지/아이콘 제거
+  // buildActivity: false, //빌드 상태 표시 없애기 (세부 설정)
+};
+
+export default nextConfig;
